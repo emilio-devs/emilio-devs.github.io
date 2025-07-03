d3bd09accf30a9916895d4d2d082bfc56727ed60
emilio-devs <ejperezmariscal@gmail.com>
2025-07-03 13:40:57 +0200

    Update base path in Vite configuration to '/emilio-devs.github.io/'

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: '/',
+    base: '/emilio-devs.github.io/',
     plugins: [vue()],
     css: {
         preprocessorOptions: {
